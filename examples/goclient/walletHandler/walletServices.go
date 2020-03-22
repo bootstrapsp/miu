90caa50464f6fe95ae4c7ddaf4446a5e66814dec
Sushant Pandey <sushant.pandey@scurid.com>
2020-03-22 14:51:25 +0100

    completing wallet services client examples with generateKey, import, export wallets.

--- examples/goclient/walletHandler/walletServices.go
+++ examples/goclient/walletHandler/walletServices.go
@@ -123,3 +123,115 @@ func CloseWallet (target string, wh int64) (closewallstatus string, message stri
 	}
 	return closewallstatus, "", nil
 }
+
+
+/*
+GenerateWalletKey() takes target and seed as input and returns generatedWalletKey or message and details on error
+
+*/
+
+func GenerateWalletKey(target, seed string) (generatedWalletKey string, message string, details []interface{}) {
+	gconn, gerr := grpcConn.GrpcConn(target)
+	if gerr != nil {
+		log.Fatal("CreateWallet rpc failed to connect see err", gerr)
+	}
+
+	wallClient := pb.NewWalletServiceClient(gconn)
+
+	gwk, err := wallClient.GenerateWalletKey(context.Background(), &pb.GenerateWalletKeyDefinition{
+		Seed: sch.ConfigProcessor().GeneratedWalletSeed,
+	})
+	generatedWalletKey = gwk.GenerateWalletKeyStatus
+	if err != nil {
+		genwallres, ok := status.FromError(err)
+		if ok {
+			log.Println(genwallres.Message())
+			log.Println(genwallres.Details())
+			return "", genwallres.Message(), genwallres.Details()
+		}
+	}
+
+	return generatedWalletKey, "", nil
+}
+
+/*
+
+ExportWallet() takes input to target and wallet handle, returns bool based confirmation on success / failure
+
+*/
+func ExportWallet(target string, wh int64) (exportWallCon bool, message string, details []interface{}, err error) {
+	gconn, gerr := grpcConn.GrpcConn(target)
+	if gerr != nil {
+		log.Fatal("CreateWallet rpc failed to connect see err", gerr)
+	}
+
+	wallClient := pb.NewWalletServiceClient(gconn)
+
+	exWalCon, err := wallClient.ExportWallet(context.Background(), &pb.ExportWalletDefinition{
+		ExportWalletHandle: wh,
+		ExportConfigJson: &pb.ExportWalletConfigJson{
+			ExportWalletPath:                sch.ConfigProcessor().WalletExportLoc,
+			ExportWalletKey:                 "SuperSecret",
+			ExportWalletKeyDerivationMethod: "ARGON2I_MOD",
+		},
+	})
+	exportWallCon = exWalCon.ExportWalletStatus
+
+	if err != nil {
+		exWalRes, ok := status.FromError(err)
+		if ok {
+			log.Println(exWalRes.Message())
+			log.Println(exWalRes.Details())
+			return exportWallCon, exWalRes.Message(), exWalRes.Details(), nil
+		}
+
+		return exportWallCon, exWalRes.Message(), exWalRes.Details(), err
+	}
+	return exportWallCon, "", nil, nil
+
+}
+
+/*
+ImportWallet (ImportWalletDefinition) returns wallet import confirmation or error with details and messages
+*/
+func ImportWallet(target string) (wallImportMsg, message string, details []interface{}) {
+	gconn, gerr := grpcConn.GrpcConn(target)
+	if gerr != nil {
+		log.Fatal("CreateWallet rpc failed to connect see err", gerr)
+	}
+
+	wallClient := pb.NewWalletServiceClient(gconn)
+
+	wallImpRes, err := wallClient.ImportWallet(context.Background(), &pb.ImportWalletDefinition{
+		Config: &pb.ImportWalletConfig{
+			Id:          sch.ConfigProcessor().WallId,
+			StorageType: "",
+			StorageConfig: &pb.ImportWalletStorageConfig{
+				Path: sch.ConfigProcessor().WalletImportLoc,
+			},
+		},
+		Credentials: &pb.ImportWalletCredentials{
+			Key:                 "SuperSecret",
+			StorageCredentials:  "",
+			KeyDerivationMethod: "ARGON2I_MOD",
+		},
+		ConfigJson: &pb.ImportWalletConfigJson{
+			Path: sch.ConfigProcessor().WalletExportLoc,
+			Key:  "SuperSecret",
+		},
+	})
+
+	wallImportMsg = wallImpRes.ImportWalletStatusCode
+	if err != nil {
+		walres, ok := status.FromError(err)
+		if ok {
+			log.Println(walres.Message())
+			log.Println(walres.Details())
+
+		}
+		return "", walres.Message(), walres.Details()
+
+	}
+
+	return wallImportMsg, "", nil
+}
