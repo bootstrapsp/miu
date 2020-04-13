a34014ce306bb5bdd7aa3b3d43e10477f6d2bf7d
Sushant Pandey <sushant.pandey@scurid.com>
2020-04-13 22:45:43 +0200

    adding further DID example services

--- examples/goclient/README.md
+++ examples/goclient/README.md
@@ -9,3 +9,13 @@ e.g. navigate to \miu\examples\goclient>
 
 protoc -I ./identitylayer ./identitylayer/identitylayer.proto --go_out=plugins=grpc:identitylayer
 ```
+
+## What's in client examples
+
+It provides sample implementation structure in Golang, how different gRPC can be invoked
+
+## What's not in client examples
+
+Currently, client only holds examples in Golang. Of course, client implementation is supported in as many languages as supported by gRPC, see https://grpc.io/docs/tutorials/ for supported languages.
+
+Also, in current state what client examples does not provide is a main function. Will add this once all the examples are covered in a sequenced call within that main.
--- examples/goclient/didHandler/didServices.go
+++ examples/goclient/didHandler/didServices.go
@@ -71,3 +71,167 @@ func ListMyDidsWithMeta(target string, walHandle int64) ([]string, error) {
 	return didList.Did, nil
 
 }
+
+// AbbreviateVerkey() takes target and verkey and returns metadata
+func AbbreviateVerkey(target, did, verkey string) (abbVerKeyMetaData string) {
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's AbbreviateVerkey() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	abbVerKey, abbVerErr := didClient.AbbreviateVerkey(context.Background(), &pb.AbbreviateVerkeyRequest{
+		Did:        "",
+		FullVerkey: "",
+	})
+
+	if abbVerErr != nil {
+		abbVerRes, ok := status.FromError(abbVerErr)
+		if ok {
+			log.Fatalln(abbVerRes.Code())
+			log.Fatalln(abbVerRes.Message())
+			log.Fatalln(abbVerRes.Details())
+			return ""
+		}
+
+	}
+	abbVerKeyMetaData = abbVerKey.Metadata
+	return abbVerKeyMetaData
+}
+
+// GetMyDidWithMeta() takes target for grpc conn, did and wallet handle and returns metadata with did + verkey
+
+func GetMyDidWithMeta(target, did string, wallHandle int64) (didWithMeta string) {
+
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's GetMyDidWithMeta() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	myDidInfo, err := didClient.GetMyDidWithMeta(context.Background(), &pb.GetMyDidWithMetaRequest{
+		WalletHandle: wallHandle,
+		Did:          did,
+	})
+	if err != nil {
+		log.Fatalln("Couldn't get proper metadata response for your DID, ", err)
+		myDIDreserr, ok := status.FromError(err)
+		if ok {
+			log.Fatalln(myDIDreserr.Code())
+			log.Fatalln(myDIDreserr.Message())
+			log.Fatalln(myDIDreserr.Details())
+		}
+
+	}
+
+	return myDidInfo.Did
+
+}
+
+// GetDidMetadata takes target for grpc client connection , did and wallet handle for fetching DID metadata
+func GetDidMetadata(target, did string, walletHandle int64) string {
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's GetDidMetadata() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	didMetaRes, err := didClient.GetDidMetadata(context.Background(), &pb.GetDidMetadataRequest{
+		WalletHandle: walletHandle,
+		Did:          did,
+	})
+
+	if err != nil {
+		log.Fatalln("Couldn't get the metadata information for DID ", err)
+		didMetaRestErr, ok := status.FromError(err)
+		if ok {
+			log.Fatalln(didMetaRestErr.Code())
+			log.Fatalln(didMetaRestErr.Message())
+			log.Fatalln(didMetaRestErr.Details())
+		}
+	}
+	return didMetaRes.Metadata
+}
+
+// SetDidMetadata() takes target , did, metadata and walletHandle as input and returns error code
+func SetDidMetadata(target, did, metadata string, walletHandle int64) int64 {
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's SetDidMetadata() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	setDidMetaRes, err := didClient.SetDidMetadata(context.Background(), &pb.SetDidMetadataRequest{
+		WalletHandle: walletHandle,
+		Did:          did,
+		Metadata:     metadata,
+	})
+
+	if err != nil {
+		setDidMetaResErr, ok := status.FromError(err)
+		if ok {
+			log.Fatalln(setDidMetaResErr.Code())
+			log.Fatalln(setDidMetaResErr.Details())
+			log.Fatalln(setDidMetaResErr.Message())
+		}
+	}
+	return setDidMetaRes.Error
+}
+
+func KeyForLocalDid(target, did string, walletHandle, pHandle int64) string {
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's SetDidMetadata() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	res, err := didClient.KeyForDid(context.Background(), &pb.KeyForDidRequest{
+		PoolHandle:   pHandle,
+		WalletHandle: walletHandle,
+		Did:          did,
+	})
+	if err != nil {
+		resErr, ok := status.FromError(err)
+		if ok {
+			log.Fatalln(resErr.Code())
+			log.Fatalln(resErr.Message())
+			log.Fatalln(resErr.Details())
+		}
+	}
+
+	return res.Key
+}
+
+func ReplaceKeysStart(target, did string, wallHandle int64) string {
+	myConn, clientErr := grpcConn.GrpcConn(target)
+	if clientErr != nil {
+		log.Println("DIDHandler's SetDidMetadata() context failed to make grpc connection", clientErr)
+		panic(clientErr)
+	}
+	didClient := pb.NewDidServiceClient(myConn)
+
+	res, err := didClient.ReplaceKeysStart(context.Background(), &pb.ReplaceKeysStartRequest{
+		WalletHandle: wallHandle,
+		Did:          did,
+		IdentityJson: &pb.ReplaceKeysStartIdentityJsonMessage{
+			Seed:       "",
+			CryptoType: "",
+		},
+	})
+	if err != nil {
+		resErr, ok := status.FromError(err)
+		if ok {
+			log.Fatalln(resErr.Code())
+			log.Fatalln(resErr.Message())
+			log.Fatalln(resErr.Details())
+		}
+	}
+
+	return res.Verkey
+
+}
